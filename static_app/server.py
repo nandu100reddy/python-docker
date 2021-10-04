14aab7b323937aa29c931f3580980320f5bfc48e
Nanda Kumar Reddy Bommakka <80082104+nandu100reddy@users.noreply.github.com>
2021-10-04 12:32:14 +0530

    Update server.py

--- static_app/server.py
+++ static_app/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():  
-    data = "Hello, World"
+    data = "WELCOME TO GOLDENT THOUGHTS"
     return render_template('index.html', data=data)
 
 # run the application
